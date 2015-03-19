842915fae87bf796d97d500f194d016bab8e23b8
Erik Hill <eghill@ucsd.edu>
2015-03-19 13:04:57 -0700

    Camera Hide player dist based on player scale.

--- Assets/Scripts/virbelacore/MainCameraController.cs
+++ Assets/Scripts/virbelacore/MainCameraController.cs
@@ -256,7 +256,7 @@ public class MainCameraController : MonoBehaviour {
         if (GameManager.Inst.LocalPlayer != null)
         {
             float distToCam = Vector3.Distance(GameManager.Inst.LocalPlayer.HeadPosition, mainCamera.transform.position);
-            float distHidePlayer = (GameManager.Inst.LevelLoaded == GameManager.Level.MOTION_TEST) ? 0.5f : 1.5f;
+            float distHidePlayer = (GameManager.Inst.LevelLoaded == GameManager.Level.MOTION_TEST || GameManager.Inst.LevelLoaded == GameManager.Level.SCALE_GAME) ? 0.5f * GameManager.Inst.LocalPlayer.gameObject.transform.localScale.x : 1.5f;
             if ((distToCam < distHidePlayer) && GameManager.Inst.LocalPlayer.Visible)
             {
                 GameManager.Inst.LocalPlayer.Visible = false;
