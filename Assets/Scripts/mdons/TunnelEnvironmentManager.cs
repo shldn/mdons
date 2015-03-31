9c867b10a1a3f496d1ba901ec73407dabe411ac4
Erik Hill <eghill@ucsd.edu>
2015-03-31 11:35:07 -0700

    Added TunnelEnvironmentManager to help reset the environment after an experiment.

---
+++ Assets/Scripts/mdons/TunnelEnvironmentManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TunnelEnvironmentManager : MonoBehaviour {
+
+    public static TunnelEnvironmentManager Inst = null;
+    
+    GameObject tunnelSpline = null;
+    GameObject tunnelDecorator = null;
+    GameObject endArrow = null;
+
+    void Awake()
+    {
+        Inst = this;
+
+        tunnelSpline = GameObject.Find("TunnelSpline");
+        tunnelDecorator = GameObject.Find("TunnelDecorator");
+        endArrow = GameObject.Find("EndArrow");
+    }
+
+    public void Reset()
+    {
+        tunnelDecorator.SetActive(true);
+        endArrow.SetActive(false);
+    }
+}
