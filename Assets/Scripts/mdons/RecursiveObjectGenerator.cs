8bf89b1113d9be85183055851746f8ae82190bce
Erik Hill <eghill@ucsd.edu>
2015-04-01 15:25:02 -0700

    RecursiveObjectGenerator - will generate the base object recursively based on a input transform.

---
+++ Assets/Scripts/mdons/RecursiveObjectGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecursiveObjectGenerator : MonoBehaviour {
+
+    GameObject baseObject = null;
+    public int numLevels = 10;
+    public Transform nextLevelTransform = null;
+
+	void Start () {
+        if (baseObject == null)
+            baseObject = gameObject;
+        if( nextLevelTransform == null )
+            nextLevelTransform = transform.Find("RecursiveTransform");
+        if (numLevels <= 0)
+            return;
+
+        GameObject child = GameObject.Instantiate(baseObject) as GameObject;
+        child.name = name;
+        child.transform.position = nextLevelTransform.position;
+        child.transform.rotation = nextLevelTransform.rotation;
+        child.transform.parent = transform;
+        child.transform.localScale = nextLevelTransform.localScale;
+        child.GetComponent<RecursiveObjectGenerator>().nextLevelTransform = child.transform.Find(nextLevelTransform.name);
+        child.GetComponent<RecursiveObjectGenerator>().numLevels = numLevels - 1;
+	}
+	
+}
