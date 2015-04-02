d9a328a8fd53845f217dfc9a9057972bcc39374d
sangel <sangel@ucsd.edu>
2015-04-02 12:48:27 -0700

    Made a script to animate the brushstrokes for the character and updated the textures.

---
+++ Assets/Scripts/AnimateBrushstrokes.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class AnimateBrushstrokes : MonoBehaviour 
+{
+
+	public Texture2D[] textures;
+	public float speed = 1;
+	int counter = 0;
+	
+	
+	// Grab the renderer
+	void Start () 
+	{
+		        		
+		//calls the changeTex function
+		changeTex();
+		
+		InvokeRepeating("changeTex", 0, speed);
+		
+    }
+	
+	//	
+    void changeTex() {
+		
+	    renderer.material.mainTexture = (textures[counter]);
+		
+		counter = (counter + 1);
+		
+		if(counter >= textures.Length){
+			counter = 0;
+		}
+	}
+    
+}
+
