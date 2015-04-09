25d995bad5bfd50f949468fa8152c10746031412
Erik Hill <eghill@ucsd.edu>
2015-04-09 09:51:44 -0700

    Fixed stutter by adding Stephanie's reduced mesh for the pink house.

--- Assets/Scripts/virbelacore/AddMeshColliderTrigger.cs
+++ Assets/Scripts/virbelacore/AddMeshColliderTrigger.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class AddMeshColliderTrigger : MonoBehaviour {
 
+    public Mesh meshToUse = null;
     void OnTriggerEnter(Collider other)
     {
-        if (GameManager.Inst.LocalPlayer != null && other.gameObject == GameManager.Inst.LocalPlayer.gameObject && gameObject.GetComponent<MeshCollider>() == null)
-            gameObject.AddComponent<MeshCollider>();
+        if (GameManager.Inst.LocalPlayer != null && other.gameObject == GameManager.Inst.LocalPlayer.gameObject && (gameObject.GetComponent<MeshCollider>() == null || gameObject.GetComponent<MeshCollider>().sharedMesh == null))
+        {
+            MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
+            if( meshCollider == null )
+                meshCollider = gameObject.AddComponent<MeshCollider>();
+            if (meshToUse != null)
+                meshCollider.sharedMesh = meshToUse;
+        }
     }
 }
