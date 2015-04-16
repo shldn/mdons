15aacc7a6becb9101a46f4fb61a16e3dadbfe5ed
Erik Hill <eghill@ucsd.edu>
2015-04-16 15:15:20 -0700

    Tunnel Experiment - Modified curve creation for more clear straight sections.

--- Assets/Scripts/mdons/SplineToTunnelTurn.cs
+++ Assets/Scripts/mdons/SplineToTunnelTurn.cs
@@ -6,6 +6,7 @@ public class SplineToTunnelTurn : MonoBehaviour {
 
     public float angle = 45.0f;
     public float curveCuspDist = 40f;
+    public float percentStraight = 0.667f; // each side will have this percentage straight
     public Vector3 startDir = -Vector3.right;
     public BezierSpline spline = null;
 
@@ -16,9 +17,9 @@ public class SplineToTunnelTurn : MonoBehaviour {
         if( angle >= 65.0f )
             Debug.LogError("SplineToTunnelTurn: Please consider an angle less than 65 degrees, the tunnel starts turning in on itself at this point.");
 
-	    if( spline.ControlPointCount != 4 )
+	    if( spline.ControlPointCount != 10 )
         {
-            Debug.LogError("SplineToTunnelTurn: spline has " + spline.ControlPointCount + " needs 4 points -- bailing...");
+            Debug.LogError("SplineToTunnelTurn: spline has " + spline.ControlPointCount + " needs 10 points -- bailing...");
             return;
         }
 
@@ -28,13 +29,34 @@ public class SplineToTunnelTurn : MonoBehaviour {
 
     void SetSplineToAngle()
     {
-        spline.SetControlPoint(1, spline.GetControlPoint(0) + curveCuspDist * startDir);
+        // start section
+
+        // main points
+        spline.SetControlPoint(1, spline.GetControlPoint(0) + 0.5f * percentStraight * curveCuspDist * startDir);
+        spline.SetControlPoint(3, spline.GetControlPoint(0) + percentStraight * curveCuspDist * startDir);
+
+        // handles
+        spline.SetControlPoint(2, spline.GetControlPoint(1));        
+        spline.SetControlPoint(4, spline.GetControlPoint(0) + curveCuspDist * startDir);
+
+
+        // end section
 
         // forming an isosolese triangle, so 2 times dist of right triangle
         float endPtDist = 2.0f * curveCuspDist * Mathf.Cos(angle * Mathf.Deg2Rad);
         Vector3 endPoint = spline.GetControlPoint(0) + endPtDist * (Quaternion.AngleAxis(angle, Vector3.up) * startDir);
-        spline.SetControlPoint(3, endPoint);
-        spline.SetControlPoint(2, spline.GetControlPoint(1));
+        Vector3 endToCuspV = spline.GetControlPoint(4) - endPoint;
+
+        // main points
+        spline.SetControlPoint(9, endPoint);
+        spline.SetControlPoint(6, endPoint + percentStraight * endToCuspV);
+
+        // handles
+        spline.SetControlPoint(8, endPoint + 0.5f * percentStraight * endToCuspV);
+        spline.SetControlPoint(7, spline.GetControlPoint(8));
+        spline.SetControlPoint(5, spline.GetControlPoint(4));
+
+
     }
 
     public void ReCompute()
