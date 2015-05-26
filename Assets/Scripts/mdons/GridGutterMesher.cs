8aa041a982df681d081febed4b492c6fe67b7f59
Erik Hill <eghill@ucsd.edu>
2015-05-26 10:18:22 -0700

    Added Recursive element to CityBlockGenerator. Recurses in the center. Added ability to hide regions of GridGutterMesher. Only working for square grids at the moment.

--- Assets/Scripts/mdons/GridGutterMesher.cs
+++ Assets/Scripts/mdons/GridGutterMesher.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GridGutterMesher{
 
-    public static void CreateMeshes(Grid2D grid, Transform parent, Texture2D mainTexture, Texture2D intersectionTexture, Texture2D holeTexture = null)
+    public static void CreateMeshes(Grid2D grid, Transform parent, Texture2D mainTexture, Texture2D intersectionTexture, Texture2D holeTexture, Bounds? ignoreBounds = null)
     {
         int rows = grid.Rows;
         int cols = grid.Cols;
@@ -17,8 +17,11 @@ public class GridGutterMesher{
                 Vector3 btmRt = To3D(grid.TopLeft(r, c));
                 Vector3 topLt = To3D(grid.BottomRight(r + 1, c));
                 Vector3 topRt = To3D(grid.TopRight(r, c));
-                GameObject planeGO = CreatePlane(btmLt, btmRt, topLt, topRt, mainTexture, "RdH-" + r + "-" + c);
-                planeGO.transform.parent = parent;
+                if (ignoreBounds != null && IsValid(ignoreBounds ?? new Bounds(), btmLt, btmRt, topLt, topRt))
+                {
+                    GameObject planeGO = CreatePlane(btmLt, btmRt, topLt, topRt, mainTexture, "RdH-" + r + "-" + c);
+                    planeGO.transform.parent = parent;
+                }
             }
         }
 
@@ -31,8 +34,11 @@ public class GridGutterMesher{
                 Vector3 btmRt = To3D(grid.BottomLeft(r, c + 1));
                 Vector3 topLt = To3D(grid.TopRight(r, c));
                 Vector3 topRt = To3D(grid.TopLeft(r, c + 1));
-                GameObject planeGO = CreatePlane(btmLt, btmRt, topLt, topRt, mainTexture, "RdV-" + r + "-" + c);
-                planeGO.transform.parent = parent;
+                if (ignoreBounds != null && IsValid(ignoreBounds ?? new Bounds(), btmLt, btmRt, topLt, topRt))
+                {
+                    GameObject planeGO = CreatePlane(btmLt, btmRt, topLt, topRt, mainTexture, "RdV-" + r + "-" + c);
+                    planeGO.transform.parent = parent;
+                }
             }
         }
 
@@ -45,8 +51,11 @@ public class GridGutterMesher{
                 Vector3 btmRt = To3D(grid.TopLeft(r, c+1));
                 Vector3 topLt = To3D(grid.BottomRight(r+1, c));
                 Vector3 topRt = To3D(grid.BottomLeft(r+1, c+1));
-                GameObject planeGO = CreatePlane(btmLt, btmRt, topLt, topRt, intersectionTexture, "RdIS-" + r + "-" + c);
-                planeGO.transform.parent = parent;
+                if (ignoreBounds != null && IsValid(ignoreBounds ?? new Bounds(), btmLt, btmRt, topLt, topRt))
+                {
+                    GameObject planeGO = CreatePlane(btmLt, btmRt, topLt, topRt, intersectionTexture, "RdIS-" + r + "-" + c);
+                    planeGO.transform.parent = parent;
+                }
             }
         }
 
@@ -61,11 +70,20 @@ public class GridGutterMesher{
                     Vector3 btmRt = To3D(grid.BottomRight(r, c));
                     Vector3 topLt = To3D(grid.TopLeft(r, c));
                     Vector3 topRt = To3D(grid.TopRight(r, c));
-                    GameObject planeGO = CreatePlane(btmLt, btmRt, topLt, topRt, holeTexture, "Block-" + r + "-" + c);
-                    planeGO.transform.parent = parent;
+                    if (ignoreBounds != null && IsValid(ignoreBounds ?? new Bounds(), btmLt, btmRt, topLt, topRt))
+                    {
+                        GameObject planeGO = CreatePlane(btmLt, btmRt, topLt, topRt, holeTexture, "Block-" + r + "-" + c);
+                        planeGO.transform.parent = parent;
+                    }
                 }
             }
         }
+    }
+
+    // Valid if it doesn't contain all of the points.
+    static bool IsValid(Bounds bounds, Vector3 pt1, Vector3 pt2, Vector3 pt3, Vector3 pt4)
+    {
+        return !(bounds.Contains(pt1) && bounds.Contains(pt2) && bounds.Contains(pt3) && bounds.Contains(pt4));
     }
 
     static GameObject CreatePlane(Vector3 btmLt, Vector3 btmRt, Vector3 topLt, Vector3 topRt, Texture2D texture, string name)
