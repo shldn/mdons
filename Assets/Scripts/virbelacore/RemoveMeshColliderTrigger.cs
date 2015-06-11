3acf45904ac2af38f0aa0bc9478a5546c0b2ebbe
Erik Hill <eghill@ucsd.edu>
2015-06-11 13:40:43 -0700

    Added script to toy prefabs to remove the mesh collider when the player leaves. -- Will help performance if world scales.

---
+++ Assets/Scripts/virbelacore/RemoveMeshColliderTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RemoveMeshColliderTrigger : MonoBehaviour
+{
+    public Mesh meshToUse = null;
+    float delaySeconds = 1;
+
+    void OnTriggerExit(Collider other)
+    {
+        if (GameManager.Inst.LocalPlayer != null && other.gameObject == GameManager.Inst.LocalPlayer.gameObject)
+            Invoke("RemoveMeshCollider", delaySeconds);
+    }
+
+    void OnTriggerEnter()
+    {
+        CancelInvoke();
+    }
+
+    void RemoveMeshCollider()
+    {
+        Destroy(gameObject.GetComponent<MeshCollider>());
+    }
+}
