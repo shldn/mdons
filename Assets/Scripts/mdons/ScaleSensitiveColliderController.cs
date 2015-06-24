c4552dc19131726e198d7ca97165be4d531d9e8d
Erik Hill <eghill@ucsd.edu>
2015-06-24 16:20:22 -0700

    Initial implementation of switch between box collider and mesh collider for toy house models.  Significant performance increase when the player is standing in the middle of the world. Player is no longer triggering mesh colliders for models he is standing on top of.

---
+++ Assets/Scripts/mdons/ScaleSensitiveColliderController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Scale Sensitive Collider
+//
+// Changes the type of collider dynamically based on the local player's relative scale to this object
+// When this object is relatively small, use a box collider
+// When it gets larger, use a trigger to add a mesh collider.
+
+public class ScaleSensitiveColliderController : MonoBehaviour {
+
+    private bool allowMeshCollisions = false;
+
+	void Start () {
+        AllowMeshCollisions = allowMeshCollisions;
+	}
+
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+            AllowMeshCollisions = !AllowMeshCollisions;
+	}
+
+    public bool AllowMeshCollisions
+    {
+        get{
+            return allowMeshCollisions;
+        }
+
+        set
+        {
+            allowMeshCollisions = value;
+            Transform container = transform.FindChild("Container");
+            if (container != null )
+                container.gameObject.SetActive(!allowMeshCollisions);
+            GetComponent<BoxCollider>().enabled = allowMeshCollisions;
+
+        }
+    }
+}
